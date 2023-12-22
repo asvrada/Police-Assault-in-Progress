9d12fb2a04baa4d991db37ce8bb2f4d5f8a742f2
Zijie Wu <noojeff@gmail.com>
2023-12-22 18:20:04 +0800

    Make it more accurate

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
     'use strict';
 
     // create font
-    var customFont = new FontFace('Custom Font', 'url(css/font/PT_Sans-Web-Regular.ttf)');
+    var customFont = new FontFace('DINEngschrift', 'url(css/font/DINEngschrift-Regular.ttf)');
     // add it to font faces
     document.fonts.add(customFont);
     // load it
@@ -27,7 +27,7 @@ function animation() {
     }
 
     // config animation, in milli sec
-    var duration = 5000;
+    var duration = 3500;
 
     // runtime
     var prevTime = new Date();
--- readme.md
+++ readme.md
@@ -1,12 +1,17 @@
 # Police Assault in Progress
 
-A recreate of the bada*s "Police Assault in Progree" indicator from Payday 2.
+A recreate of the "Police Assault in Progress" indicator from Payday 2.
 
 [Demo](https://asvrada.github.io/Police-Assault-in-Progress/)
 
 ## Getting Started
 
 Clone this repo and open index.html with browser of your choice, and there it is!
+
+## For developer
+
+1. Download https://sass-lang.com/
+2. `sass index.scss index.css`
 
 ## License
 
